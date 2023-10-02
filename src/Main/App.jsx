2aed1360688bdc723fd4bef80e3fec241a429607
sina <sinaheidarian21@gmail.com>
2023-10-02 15:57:05 +0330

    first

---
+++ src/Main/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css"
+
+const App = () => {
+  return (
+    <div className="main">
+        <p>hiiiiiiiii</p>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Navbar.css"
+
+const Navbar = () => {
+  return (
+    <div className='Navbar'>
+        <p>Navbar</p>
+    </div>
+  );
+};
+
+export default Navbar;
