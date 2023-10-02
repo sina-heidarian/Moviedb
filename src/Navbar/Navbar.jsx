8abb69a381f12e69155c88babefb59fbb167a3bc
sina <sinaheidarian21@gmail.com>
2023-10-02 19:20:29 +0330

    day 1

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,10 +1,28 @@
 import React from "react";
 import "./Navbar.css"
+import YourSvg from"./logo.svg"
 
 const Navbar = () => {
   return (
     <div className='Navbar'>
-        <p>Navbar</p>
+      <div className="content">
+        <div className="sub_media">
+          <div className="nav">
+            <a className="logo" href="/">
+              <img src={YourSvg} alt="The Movie Database" />
+            </a>
+            <ul className="navigation">
+              <li>Movies</li>
+              <li>TV Shows</li>
+              <li>People</li>
+              <li>More</li>
+            </ul>
+          </div>
+          <div className="flex">
+            <ul></ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
